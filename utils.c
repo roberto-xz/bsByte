64a9b4e4bdb5aad72ff1fcbbd7eaee031405ab96
roberto-xz <devv-robert@outlook.com>
2025-06-08 22:41:56 -0300

    modificado

--- utils.c
+++ utils.c
@@ -1,33 +1,32 @@
 
 //
-// autor: roberto-xz 07, Junho 2025 
+// autor: roberto-xz 07, Junho 2025
 //
 
-#include <ctype.h>
-#include <string.h>
 #include "./structs/command_parts.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
-char *trim (char *str) {
-    if (*str == 0 ) return str; // caso a string seja vazia
-    while (isspace((unsigned char)*str)) { str++; } // incrementa o ponteiro sempre que encontrar um espaço em branco
-    
+char* trim(char* str) {
+    if (*str == 0) return str; // caso a string seja vazia
+
+    // incrementa o ponteiro sempre que encontrar um espaço em branco
+    while (isspace((unsigned char)*str)) { str++;}
+
     // calucula o endereço final da string
-    char *end = str + strlen(str) - 1;
+    char* end = str + strlen(str) - 1;
 
-    while (end > str && isspace((unsigned char)*end)) {
-        // enquanto for um espaço, decrementa o endereço final
-        end--;
-    }
+    // enquanto for um espaço, decrementa o endereço final
+    while (end > str && isspace((unsigned char)*end)) { end--;}
     *(end + 1) = 0;
     return str;
 }
 
-command_parts split_line(char *str) {
+command_parts split_line(char* str) {
     command_parts parts;
-    parts.part_1 = "lda";
-    parts.part_2 = "@1";
-    parts.part_3 = "#0x0111";
+    parts.part_1 = strtok(str, " ");
+    parts.part_2 = strtok(NULL," ");
+    parts.part_3 = strtok(NULL," ");
     return parts;
 }
-
-

f1636b0ae1b6f31dc5faadf1bae69d70fe7ab353
roberto-xz <devv-robert@outlook.com>
2025-06-10 01:08:38 -0300

    add: adicionado validação da parte 3, e melhorando a validação da parte 1

--- lexer.c
+++ lexer.c
@@ -14,7 +14,7 @@
 *
 * função para analisar a primeira parte de uma instrução, (mnemónico)
 * 1° verifica se possue 3 bytes, número máximo permitido
-* 2° verifica se é um mnemónico válido.
+* 2° verifica se é um mnemónico válido. (Se pertence as palavras reservadas)
 */
 int analize_part_1(char part_1[], int line) {
     if (strlen(part_1) < 3 || strlen(part_1) > 3 ) {
@@ -26,19 +26,27 @@ int analize_part_1(char part_1[], int line) {
         printf("lexer-error: invalid mnemonio %s line: %d\n",part_1, line);
         return 1;
     }
+
     return 0;
 }
-
 
 /*
  *
  * função para analisar a segunda parte de uma instrução
  *
+ * 1° verifica se é um jump.
+ *      1° verifica o tipo de salto, endereço ou label
+ *      2° verifica se existe o label
+ *
+ * 2° verifica se uma instrução mov, ou say
+ *      1° verifica se o primerio argumento é um accumulator válido
+ *      2° verifica se o primeiro argumento é um endereço válido
+ *
  */
 int analize_part_2(char part_1[], char part_2[], int line, char labels[20][20],int label_count) {
     if (strcmp(part_1,"jpm") == 0) {
         if (part_2[0] == '#') {
-            if (strlen(part_2) < 6 || strlen(part_2) > 6 ) {
+            if (strlen(part_2) < 7 || strlen(part_2) > 7 ) {
                 printf("lexer-error: invalid jump adress %s: line -> %d\n", part_2, line);
                 return 1;
             }
@@ -46,8 +54,9 @@ int analize_part_2(char part_1[], char part_2[], int line, char labels[20][20],i
 
         if (part_2[0] == '$') {
             short label_is_declared = 0;
-            for (int x=0; x<label_count; x++)
+            for (int x=0; x<label_count; x++){
                 if (strstr(labels[x],part_2)) label_is_declared = 1;
+            }
 
             if (label_is_declared == 0 ) {
                 printf("lexer-error: label %s not declared: line -> %d\n", part_2, line);
@@ -57,11 +66,77 @@ int analize_part_2(char part_1[], char part_2[], int line, char labels[20][20],i
     }
 
     if (strcmp(part_1,"mov") == 0 || strcmp(part_1,"say") == 0 ) {
-        if (part_2[0] == '@') {} // registrador
-        if (part_2[0] == '#') {} // endereço
+
+        if (part_2[0] == '@') {
+
+            if (strlen(part_2) < 2 || strlen(part_2) > 2 ) {
+                printf("lexer-error: invalid accumulator adress %s: line -> %d\n", part_2, line);
+                return 1;
+            }
+
+            char ac_value[2] = {part_2[1],'\0'};
+            printf("%d",atoi(ac_value));
+            if (atoi(ac_value) < 0 || atoi(ac_value) > 4 ) {
+                printf("lexer-error: invalid accumulator adress %s: line -> %d\n", part_2, line);
+                return 1;
+            }
+        }
+        if (part_2[0] == '#') {
+            if (strlen(part_2) < 6 || strlen(part_2) > 6 ) {
+                printf("lexer-error: invalid adress %s: line -> %d\n", part_2, line);
+                return 1;
+            }
+        }
     }
 
+    if (strlen(part_2) < 2 || strlen(part_2) > 2 ) {
+        printf("lexer-error: invalid accumulator adress %s: line -> %d\n", part_2, line);
+        return 1;
+    }
+
+    char ac_value[2] = {part_2[1],'\0'};
+    if (atoi(ac_value) < 0 || atoi(ac_value) > 4 ) {
+        printf("lexer-error: invalid accumulator adress %s: line -> %d\n", part_2, line);
+        return 1;
+    }
     return 0;
+}
+
+int analize_part_3(char part_1[], char part_3[], int line){
+    if (strcmp(part_1,"jpm") == 0 || strcmp(part_1,"say") == 0) return 0;
+    if (strcmp(part_1,"mov") == 0 ){
+        if (part_3[0] == '#'){
+            if (strlen(part_3) < 6 || strlen(part_3) > 6 ) {
+                printf("lexer-error: invalid adress %s: line -> %d\n", part_3, line);
+                return 1;
+            }
+        }
+        if (part_3[0] == '@'){
+            if (strlen(part_3) < 2 || strlen(part_3) > 2 ) {
+                printf("lexer-error: invalid accumulator adress %s: line -> %d\n", part_3, line);
+                return 1;
+            }
+
+            char ac_value[2] = {part_3[1],'\0'};
+            if (atoi(ac_value) < 0 || atoi(ac_value) > 4 ) {
+                printf("lexer-error: invalid accumulator adress %s: line -> %d\n", part_3, line);
+                return 1;
+            }
+        }
+    }
+
+    if (strcmp(part_1,"set") == 0){
+        if (strlen(part_3) < 6 || strlen(part_3) > 6 ) {
+            printf("lexer-error: invalid hex(6) value %s: line -> %d\n", part_3, line);
+            return 1;
+        }
+    }
+
+    if (strlen(part_3) < 6 || strlen(part_3) > 6 ) {
+        printf("lexer-error: adress %s: line -> %d\n", part_3, line);
+        return 1;
+    }
+    return 1;
 }
 
 int lexer(char* filename) {
@@ -96,12 +171,13 @@ int lexer(char* filename) {
             if (line[0] == ':') {
                 strncpy(labels[label_count], strtok(line, " "), 19);
                 labels[label_count][19] = '\0';
+                labels[label_count][0] = '$';
                 label_count +=1;
             }else {
                 command_parts cmd_parts = split_line(line);
-                if (analize_part_1(cmd_parts.part_1, lines_count) == 1) return 1;
-                if (analize_part_2(cmd_parts.part_1,cmd_parts.part_2, lines_count,labels,label_count) == 1) return 1;
-                // break;
+                if (analize_part_1(cmd_parts.part_1, lines_count) == 1) {return 1;}
+                if (analize_part_2(cmd_parts.part_1,cmd_parts.part_2, lines_count,labels,label_count) == 1){return 1;}
+                //if (analize_part_3(cmd_parts.part_1, cmd_parts.part_3,lines_count) == 1) return 1;
             }
         }
         lines_count += 1;
@@ -114,5 +190,6 @@ int lexer(char* filename) {
 
 int main() {
     lexer("exemplos/simple_sum.bs");
+
     return 0;
 }

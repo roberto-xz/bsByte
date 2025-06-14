69139b040cafd6ec233addcd893e84189924612d
roberto-xz <devv-robert@outlook.com>
2025-06-14 14:50:58 -0300

    adicionado validação de valores hex

--- lexer_utils/valid_adress.c
+++ lexer_utils/valid_adress.c
@@ -5,15 +5,17 @@
 */
 
 #include <string.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 short valid_adress(char *adress) {
     if (adress == NULL) return 1;
-    if (strlen(adress) < 7 || strlen(adress) > 7) return 1;
+    if (strlen(adress) != 7) return 1;
 
-    // converter o hex para dec, e verificar se está no limite de intervalo
-    // permitido.
+    unsigned int hex_val = (unsigned int) strtol(adress + 1, NULL, 16);
+    if (hex_val > 0x1FFFF) return 1;
+
+    if (hex_val & (1 << 16)) return 1; // rejeita negativos
 
     return 0;
 }
--- lexer_utils/valid_stvalue.c
+++ lexer_utils/valid_stvalue.c
@@ -5,15 +5,19 @@
 */
 
 #include <string.h>
-#include <string.h>
 #include <stdlib.h>
 
 short valid_stvalue(char *value) {
     if (value == NULL) return 1;
-    if (strlen(value) < 6 || strlen(value) > 6) return 1;
+    if (strlen(value) != 6 ) return 1;
 
-    // converter o hex para dec, e verificar se está no limite de intervalo
-    // permitido.
+    unsigned int hex_val = (unsigned int) strtol(value, NULL, 16);
+    if (hex_val > 0x1FFFF) return 1;
+
+    int sign_bit = (hex_val & (1 << 16)) != 0;
+    int signed_val = sign_bit ? hex_val - (1 << 17) : hex_val;
+
+    if (signed_val < -65536 || signed_val > 65535) return 1;
 
     return 0;
 }

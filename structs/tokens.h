0f0fef99c4a6a2887405a401a8f75df39a3684bb
roberto-xz <devv-robert@outlook.com>
2025-06-19 23:36:45 -0300

    adicionado atributos para linhas e colunas

--- structs/tokens.h
+++ structs/tokens.h
@@ -6,9 +6,10 @@
 
 
 struct token_node {
-    char *token;
-    char *value;
-    int    line;
+    char token[300];
+    char value[50];
 
+    int    line;
+    int    coll;
     struct token_node *nex_node;
 };
